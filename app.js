e9bef133f502008eea378ddb1715537b824de49f
emmanuel <mercuremekinda@gmail.com>
2022-11-14 14:57:14 +0100

    Affichage du serpen et de la zone de jeu

---
+++ app.js
@@ -0,0 +1,19 @@
+let zone = document.getElementById("zone");
+zone.style.width = "500px";
+zone.style.height = "500px";
+zone.style.position = "absolute";
+zone.style.top = "50%";
+zone.style.left = "55%";
+zone.style.transform = "translate(-50%,-50%)";
+zone.style.border = "2px solid black";
+zone.style.backgroundColor = 'darkslategrey';
+   
+
+let snake = document.getElementById("snake");
+snake.style.width= "20px";
+snake.style.height= "20px";
+snake.style.backgroundColor= 'yellow';
+
+
+
+
